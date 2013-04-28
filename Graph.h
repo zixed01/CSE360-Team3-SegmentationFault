a227deacdc0b6adf895399f4c45339fa28cafa8e
Lauren Walker <lewalke3@asu.edu>
2013-04-27 20:10:04 -0700

    Graph
    Changed so takes SessionData as a parameter, and also plots the goals,
    changes title etc.

--- Graph.h
+++ Graph.h
@@ -6,7 +6,7 @@ using namespace std;
 using namespace System;
 
 void MarshalString ( String ^ s, string& os );
-namespace PulseVisualJ {
+namespace graph2 {
 
 	using namespace System;
 	using namespace System::ComponentModel;
@@ -24,12 +24,14 @@ namespace PulseVisualJ {
 	{
 	public:
 		
-		Graph(void)
+		Graph(SessionData ^ s)
 		{
 			InitializeComponent();
+			this->Show();
 			//
 			//TODO: Add the constructor code here
 			//
+			session = s;
 		}
 
 
@@ -48,23 +50,27 @@ namespace PulseVisualJ {
 
 	private: System::Windows::Forms::DateTimePicker^  dateTimePicker1;
 	protected: 
-	private: System::Windows::Forms::DateTimePicker^  dateTimePicker2;
-	private: System::Windows::Forms::Label^  label1;
-	private: System::Windows::Forms::Label^  label2;
+
+
+
 	private: System::Windows::Forms::DataVisualization::Charting::Chart^  chart1;
-	private: System::Windows::Forms::Button^  button1;
-	private: System::Windows::Forms::Label^  label3;
+
+
 	private: System::Windows::Forms::TextBox^  textBox1;
 	private: System::Windows::Forms::ComboBox^  comboBox1;
 	private: System::Windows::Forms::Button^  button2;
 
 	private: System::Windows::Forms::Label^  label4;
+	private: System::Windows::Forms::DataVisualization::Charting::Chart^  chart2;
+	private: System::Windows::Forms::Label^  label5;
 
 	private:
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
 		System::ComponentModel::Container ^components;
+	private:
+		SessionData ^ session;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -74,108 +80,41 @@ namespace PulseVisualJ {
 
 		void InitializeComponent(void)
 		{
-			System::Windows::Forms::DataVisualization::Charting::ChartArea^  chartArea3 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
-			System::Windows::Forms::DataVisualization::Charting::Legend^  legend3 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
+			System::Windows::Forms::DataVisualization::Charting::ChartArea^  chartArea2 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
+			System::Windows::Forms::DataVisualization::Charting::Legend^  legend2 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
 			System::Windows::Forms::DataVisualization::Charting::Series^  series3 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
-			System::Windows::Forms::DataVisualization::Charting::Title^  title3 = (gcnew System::Windows::Forms::DataVisualization::Charting::Title());
+			System::Windows::Forms::DataVisualization::Charting::Series^  series4 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
 			this->dateTimePicker1 = (gcnew System::Windows::Forms::DateTimePicker());
-			this->dateTimePicker2 = (gcnew System::Windows::Forms::DateTimePicker());
-			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->chart1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
-			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->label4 = (gcnew System::Windows::Forms::Label());
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->chart1))->BeginInit();
+			this->chart2 = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->chart2))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// dateTimePicker1
 			// 
-			this->dateTimePicker1->Location = System::Drawing::Point(31, 47);
+			this->dateTimePicker1->Location = System::Drawing::Point(12, 12);
 			this->dateTimePicker1->Name = L"dateTimePicker1";
 			this->dateTimePicker1->Size = System::Drawing::Size(200, 20);
 			this->dateTimePicker1->TabIndex = 0;
 			this->dateTimePicker1->ValueChanged += gcnew System::EventHandler(this, &Graph::dateTimePicker1_ValueChanged);
 			// 
-			// dateTimePicker2
-			// 
-			this->dateTimePicker2->Location = System::Drawing::Point(31, 96);
-			this->dateTimePicker2->Name = L"dateTimePicker2";
-			this->dateTimePicker2->Size = System::Drawing::Size(200, 20);
-			this->dateTimePicker2->TabIndex = 1;
-			this->dateTimePicker2->ValueChanged += gcnew System::EventHandler(this, &Graph::dateTimePicker2_ValueChanged);
-			// 
-			// label1
-			// 
-			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(31, 31);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(59, 13);
-			this->label1->TabIndex = 2;
-			this->label1->Text = L"From Date:";
-			// 
-			// label2
-			// 
-			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(31, 80);
-			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(49, 13);
-			this->label2->TabIndex = 3;
-			this->label2->Text = L"To Date:";
-			// 
-			// chart1
-			// 
-			chartArea3->Name = L"ChartArea1";
-			this->chart1->ChartAreas->Add(chartArea3);
-			legend3->Name = L"Legend1";
-			this->chart1->Legends->Add(legend3);
-			this->chart1->Location = System::Drawing::Point(268, 12);
-			this->chart1->Name = L"chart1";
-			series3->ChartArea = L"ChartArea1";
-			series3->Legend = L"Legend1";
-			series3->Name = L"Series1";
-			this->chart1->Series->Add(series3);
-			this->chart1->Size = System::Drawing::Size(355, 230);
-			this->chart1->TabIndex = 4;
-			this->chart1->Text = L"Graph";
-			title3->Name = L"Graph";
-			title3->Text = L"Graph";
-			this->chart1->Titles->Add(title3);
-			// 
-			// button1
-			// 
-			this->button1->Location = System::Drawing::Point(31, 222);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(81, 24);
-			this->button1->TabIndex = 5;
-			this->button1->Text = L"Go Back";
-			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &Graph::button1_Click);
-			// 
-			// label3
-			// 
-			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(33, 179);
-			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(57, 13);
-			this->label3->TabIndex = 6;
-			this->label3->Text = L"Add Goal: ";
-			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(131, 176);
+			this->textBox1->Location = System::Drawing::Point(442, 11);
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(100, 20);
 			this->textBox1->TabIndex = 7;
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &Graph::textBox1_TextChanged);
 			// 
 			// comboBox1
 			// 
 			this->comboBox1->FormattingEnabled = true;
 			this->comboBox1->Items->AddRange(gcnew cli::array< System::Object^  >(3) {L"Weight", L"Blood Pressure", L"Sugar Level"});
-			this->comboBox1->Location = System::Drawing::Point(110, 134);
+			this->comboBox1->Location = System::Drawing::Point(315, 10);
 			this->comboBox1->Name = L"comboBox1";
 			this->comboBox1->Size = System::Drawing::Size(121, 21);
 			this->comboBox1->TabIndex = 8;
@@ -183,7 +122,7 @@ namespace PulseVisualJ {
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(156, 223);
+			this->button2->Location = System::Drawing::Point(548, 12);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(75, 23);
 			this->button2->TabIndex = 9;
@@ -194,31 +133,59 @@ namespace PulseVisualJ {
 			// label4
 			// 
 			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(31, 137);
+			this->label4->Location = System::Drawing::Point(247, 14);
 			this->label4->Name = L"label4";
 			this->label4->Size = System::Drawing::Size(62, 13);
 			this->label4->TabIndex = 11;
 			this->label4->Text = L"Graph Data";
+			this->label4->Click += gcnew System::EventHandler(this, &Graph::label4_Click);
+			// 
+			// chart2
+			// 
+			chartArea2->Name = L"ChartArea1";
+			this->chart2->ChartAreas->Add(chartArea2);
+			legend2->Name = L"Legend1";
+			this->chart2->Legends->Add(legend2);
+			this->chart2->Location = System::Drawing::Point(7, 43);
+			this->chart2->Name = L"chart2";
+			this->chart2->Palette = System::Windows::Forms::DataVisualization::Charting::ChartColorPalette::Fire;
+			series3->ChartArea = L"ChartArea1";
+			series3->ChartType = System::Windows::Forms::DataVisualization::Charting::SeriesChartType::Line;
+			series3->Legend = L"Legend1";
+			series3->Name = L"Data";
+			series4->ChartArea = L"ChartArea1";
+			series4->ChartType = System::Windows::Forms::DataVisualization::Charting::SeriesChartType::Line;
+			series4->Legend = L"Legend1";
+			series4->Name = L"Goal";
+			this->chart2->Series->Add(series3);
+			this->chart2->Series->Add(series4);
+			this->chart2->Size = System::Drawing::Size(616, 209);
+			this->chart2->TabIndex = 12;
+			this->chart2->Text = L"chart2";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(259, 43);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(41, 13);
+			this->label5->TabIndex = 13;
+			this->label5->Text = L"Weight";
 			// 
 			// Graph
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(635, 264);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->chart2);
 			this->Controls->Add(this->label4);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->comboBox1);
 			this->Controls->Add(this->textBox1);
-			this->Controls->Add(this->label3);
-			this->Controls->Add(this->button1);
-			this->Controls->Add(this->chart1);
-			this->Controls->Add(this->label2);
-			this->Controls->Add(this->label1);
-			this->Controls->Add(this->dateTimePicker2);
 			this->Controls->Add(this->dateTimePicker1);
 			this->Name = L"Graph";
-			this->Text = L"Graph";
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->chart1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->chart2))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -231,34 +198,73 @@ private: System::Void button1_Click(System::Object^  sender, System::EventArgs^ 
 
 			  //Stats ^ stats = gcnew Stats();
 			  //stats->Show();
-              Graph::Close();
+              //Graph::Close();
 		 }
 private: System::Void dateTimePicker1_ValueChanged(System::Object^  sender, System::EventArgs^  e) {
 			 String ^ tempstartdate =  dateTimePicker1->Value.ToString("MM/dd/yyyy");
-			 string startdate;
-			 MarshalString(tempstartdate, startdate);
+			 //dateTimePicker1->Value = DateTime->Now->AddDays(7);
+			 //string startdate;
+			 //MarshalString(tempstartdate, startdate);
 			 //Pull all stats from this date to end date and display them on the graph and goal line if applicable
 
 		 }
 private: System::Void dateTimePicker2_ValueChanged(System::Object^  sender, System::EventArgs^  e) {
-			 String^ tempenddate =  dateTimePicker2->Value.ToString("MM/dd/yyyy");
-			 string enddate;
-			 MarshalString(tempenddate, enddate);
+			 //String^ tempenddate =  dateTimePicker2->Value.ToString("MM/dd/yyyy");
+			 //string enddate;
+			 //MarshalString(tempenddate, enddate);
 			 //Change end date of graph
 		 }
 
 private: System::Void comboBox1_SelectedIndexChanged(System::Object^  sender, System::EventArgs^  e) 
 		 {
+			 if(comboBox1->SelectedIndex == 0)
+			 {
+				 this->label5->Text = L"Weight";
+				 this->chart2->Series[0]->Points->Clear();
+				 this->chart2->Series[1]->Points->Clear();
+			 }
+			 else if(comboBox1->SelectedIndex == 1)
+			 {
+				 this->label5->Text = L"Blood Pressure";
+				 this->chart2->Series[0]->Points->Clear();
+				 this->chart2->Series[1]->Points->Clear();
+			 }
+			 else if(comboBox1->SelectedIndex == 2)
+			 {
+				 this->label5->Text = L"Sugar Level";
+				 this->chart2->Series[0]->Points->Clear();
+				 this->chart2->Series[1]->Points->Clear();
+			 }
 		 }
 private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) 
 		 {
-			 String ^ temp = textBox1->Text; 
-			 string goal;
-			 MarshalString(temp, goal);
+			 String ^ temp = textBox1->Text;
+			 if(temp != "")
+			 {
+			 int goal = int::Parse(temp);
+			 if(goal != 0)
+			 {
+				 this->chart2->Series[1]->Points->AddXY(0,goal);
+				 this->chart2->Series[1]->Points->AddXY(14,goal);
+			 }
+			 }
+			 /*this->chart2->Series[0]->Points->AddXY(1,25);
+			 this->chart2->Series[0]->Points->AddXY(2,10);
+			 this->chart2->Series[0]->Points->AddXY(5,5);
+			 this->chart2->Series[0]->Points->AddXY(9,5);
+			 this->chart2->Series[0]->Points->AddXY(15,5);*/
+			 //string goal;
+			 //MarshalString(temp, goal);
 
 			 //Save Goal in database along with goaltype
 			 //Refresh graph
 		 }
 
+private: System::Void label3_Click(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void label4_Click(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void textBox1_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
 };
 }
